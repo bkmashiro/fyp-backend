8a3b881c2a2a393269414ef5199cb6c062af6a8f
bkmashiro <53376445+bkmashiro@users.noreply.github.com>
2025-01-26 22:58:20 +0000

    add search geo object

--- src/modules/geo-object/geo-object.service.ts
+++ src/modules/geo-object/geo-object.service.ts
@@ -1,26 +1,55 @@
-import { Injectable } from '@nestjs/common';
-import { CreateGeoObjectDto } from './dto/create-geo-object.dto';
-import { UpdateGeoObjectDto } from './dto/update-geo-object.dto';
+import { Injectable } from '@nestjs/common'
+import { CreateGeoObjectDto } from './dto/create-geo-object.dto'
+import { UpdateGeoObjectDto } from './dto/update-geo-object.dto'
+import { GeoObject } from './entities/geo-object.entity'
+import { InjectRepository } from '@nestjs/typeorm'
+import { Repository } from 'typeorm'
 
 @Injectable()
 export class GeoObjectService {
+  constructor(
+    @InjectRepository(GeoObject)
+    private geoObjectRepository: Repository<GeoObject>,
+  ) {}
+
   create(createGeoObjectDto: CreateGeoObjectDto) {
-    return 'This action adds a new geoObject';
+    return 'This action adds a new geoObject'
   }
 
   findAll() {
-    return `This action returns all geoObject`;
+    return `This action returns all geoObject`
   }
 
   findOne(id: number) {
-    return `This action returns a #${id} geoObject`;
+    return `This action returns a #${id} geoObject`
   }
 
   update(id: number, updateGeoObjectDto: UpdateGeoObjectDto) {
-    return `This action updates a #${id} geoObject`;
+    return `This action updates a #${id} geoObject`
   }
 
   remove(id: number) {
-    return `This action removes a #${id} geoObject`;
+    return `This action removes a #${id} geoObject`
+  }
+
+  async findObjectsInArea(
+    lat: number,
+    lon: number,
+    alt: number,
+    radius: number,
+  ) {
+    const objects = await this.geoObjectRepository
+      .createQueryBuilder('object')
+      .where(
+        `ST_DWithin(
+          object.location::geography, 
+          ST_SetSRID(ST_MakePoint(:lon, :lat), 4326)::geography, 
+          :radius
+        )`,
+        { lat, lon, alt, radius },
+      )
+      .getMany()
+
+    return objects
   }
 }
