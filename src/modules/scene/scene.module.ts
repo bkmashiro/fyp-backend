90b9eace63c47dc195c2f83bb31d0eaf41870899
bkmashiro <53376445+bkmashiro@users.noreply.github.com>
2025-03-09 10:17:56 +0000

    add scene

--- src/modules/scene/scene.module.ts
+++ src/modules/scene/scene.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { SceneService } from './scene.service';
 import { SceneController } from './scene.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Scene } from './entities/scene.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Scene])],
   controllers: [SceneController],
   providers: [SceneService],
 })
--- src/modules/scene/scene.service.ts
+++ src/modules/scene/scene.service.ts
@@ -1,8 +1,45 @@
 import { Injectable } from '@nestjs/common';
 import { CreateSceneDto } from './dto/create-scene.dto';
 import { UpdateSceneDto } from './dto/update-scene.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Scene } from './entities/scene.entity';
 
 @Injectable()
 export class SceneService {
+  constructor(
+    @InjectRepository(Scene)
+    private sceneRepository: Repository<Scene>,
+  ) {}
 
+  async create(createSceneDto: CreateSceneDto) {
+    const scene = this.sceneRepository.create(createSceneDto);
+    return this.sceneRepository.save(scene);
+  }
+
+  findAll() {
+    return this.sceneRepository.find({
+      relations: ['children'],
+    });
+  }
+
+  findOne(id: string) {
+    return this.sceneRepository.findOne({
+      where: { id },
+      relations: ['children'],
+    });
+  }
+
+  async update(id: string, updateSceneDto: UpdateSceneDto) {
+    await this.sceneRepository.update(id, updateSceneDto);
+    return this.findOne(id);
+  }
+
+  async remove(id: string) {
+    const scene = await this.findOne(id);
+    if (scene) {
+      await this.sceneRepository.remove(scene);
+    }
+    return scene;
+  }
 }
